8ef9450efabd39355df8da9ab867d94c67b427f9
Ismael <ismaelturtle@gmail.com>
2023-10-19 14:46:00 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- Nav bar -->
+    <nav class="navbar navbar-expand navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand float-left" href="/">CIS 4339 Midterm</a>
+        <ul class="nav navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/form">Form</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Router view - our main view below the navigation -->
+    <div class="container mt-5">
+      <RouterView></RouterView>
+    </div>
+  </div>
+</template>
+
+
---
+++ src/views/FormView.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="row justify-content-center">
+        <div class="col-md-6">
+            <h2 class="text-center">Weather Data</h2>
+            <h3 class="text-center">Please fill in the form and use the submit button to load data.</h3>
+            <!-- Weather Query Form -->
+            <form @submit.prevent="handleUpdateForm">
+                <div class="form-group">
+                    <label>Location*</label>
+                    <input type="text" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Query Start Date*</label>
+                    <input type="date" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Query End Date*</label>
+                    <input type="date" class="form-control" required>
+                </div>
+
+                <button class="btn btn-danger mt-3">Get Data</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'FormView',
+}
+</script>
